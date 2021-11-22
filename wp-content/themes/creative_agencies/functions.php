7075d62c053a5174ff5e1cb9905eac32095f883d
Freench <f.pernot@codeur.online>
2021-11-22 14:04:52 +0100

    Section 3

--- wp-content/themes/creative_agencies/functions.php
+++ wp-content/themes/creative_agencies/functions.php
@@ -62,3 +62,67 @@ function custom_post_type_projects() {
 }
 
 add_action( 'init', 'custom_post_type_projects', 0 );
+
+
+function footer_widgets_init() {
+
+	register_sidebar( array(
+
+	'name' => 'Footer Widgets Area',
+	'id' => 'footer-widgets-area',
+	'before_widget' => '<div class="footer-widgets-area">',
+	'after_widget' => '</div>',
+	'before_title' => '<h3>',
+	'after_title' => '</h3>',
+	) );
+   }
+
+add_action( 'widgets_init', 'footer_widgets_init' );
+
+class hstngr_widget extends WP_Widget {
+
+	//Insert functions here
+	function __construct() {
+		parent::__construct(
+		// widget ID
+		'footer_widget',
+		// widget name
+		__('Footer Widget', ' hstngr_widget_domain'),
+		// widget description
+		array( 'description' => __( 'Hostinger Widget Tutorial', 'hstngr_widget_domain' ), )
+		);
+	}
+
+	public function widget( $args, $instance ) {
+		$title = apply_filters( 'widget_title', $instance['title'] );
+		echo $args['before_widget'];
+		//if title is present
+		if ( ! empty( $title ) )
+		echo $args['before_title'] . $title . $args['after_title'];
+		//output
+		echo __( 'Hello, World from Hostinger.com', 'hstngr_widget_domain' );
+		echo $args['after_widget'];
+	}
+
+	public function form( $instance ) {
+		if ( isset( $instance[ 'title' ] ) )
+		$title = $instance[ 'title' ];
+		else
+		$title = __( 'Default Title', 'hstngr_widget_domain' );
+		?>
+		<p>
+		<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label>
+		<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
+		</p>
+		<?php
+	}
+	public function update( $new_instance, $old_instance ) {
+		$instance = array();
+		$instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
+		return $instance;
+	}
+}
+function hstngr_register_widget() {
+	register_widget( 'hstngr_widget' );
+}
+add_action( 'widgets_init', 'hstngr_register_widget' );
---
+++ wp-content/themes/creative_agencies/section3.php
@@ -0,0 +1,41 @@
+<?php
+    
+$args = array(
+    'post_type'=> 'projects',
+    'posts_per_page' => '2',
+    'order'    => 'DESC'
+);
+
+$the_query = new WP_Query( $args );
+if($the_query->have_posts() ) : 
+    $i=-1;
+    while ( $the_query->have_posts() ) : 
+        $i+=1;
+        $the_query->the_post(); 
+
+        // echo $the_query -> post_title;
+        if(have_rows('gallery_projects')){
+            $gallery = get_field('gallery_projects');
+            //print_r($gallery);
+        }
+        ?>
+        <div class="s3-project">
+            <div class="s3-project-content">
+                <h3><?= $the_query->posts[$i]->post_title; ?></h3>
+                <p><?= $the_query->posts[$i]->post_content ; ?></p>
+            </div>
+            <div class="s3-img-container">
+            
+                <img src="<?= $gallery[0]['image_gallery'] ; ?>" alt="Une image">
+            </div>
+        </div>
+
+        <?php
+
+        // content goes here
+    endwhile; 
+    wp_reset_postdata(); 
+else: 
+endif;
+
+?>
