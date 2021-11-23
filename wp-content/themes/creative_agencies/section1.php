e010449b75b17f8b042b1e05e7b7da5538b9881f
Plumtree3D <ham.in.kneesocks@gmail.com>
2021-11-23 14:28:33 +0100

    working images gallery in single.php

--- wp-content/themes/creative_agencies/section1.php
+++ wp-content/themes/creative_agencies/section1.php
@@ -9,10 +9,16 @@ $text_scroll_button_section_1 = get_field("text_scroll_button_section_1");
     <div class="backgroundImage">
         <img src="<?=$background_image_section_1?>" alt="">
     </div>
-<h1 id="page_title"> <?=$title_section_1?> </h1>
+  
+        <h1 id="page_title"> <?=$title_section_1?> </h1>
 
-    <div class="scrollDown">
-        <span class="iconify" data-icon="mdi:chevron-down"></span>
-    </div>
+        <span id="scrollBtnDesc"> <?=$text_scroll_button_section_1?> </span>
+        <a href="#section2">
+        <div class="scrollDown">
+            <span class="iconify" data-icon="mdi:chevron-down"></span>
+        </div>
+        </a>
+
+
 
 </div>
--- wp-content/themes/creative_agencies/single.php
+++ wp-content/themes/creative_agencies/single.php
@@ -1,10 +1,10 @@
 <?php get_header(); ?>
-<div class="bg">
+
     <div class="main single">
 
-    <?php if (have_posts()) : ?>
-    <?php while (have_posts()) : the_post(); ?>
-    <div class="post">
+    <?php if (have_posts()) { ?>
+    <?php while (have_posts()) { the_post(); ?>
+
         <h1 class="post-title"><?php the_title(); ?></h1>
 
         <div class="singleContainer">
@@ -13,12 +13,18 @@
                 if(!empty($gallery)) {
                     foreach ($gallery as $picture) { ?>
 
-                    <div class="singleSlide">
+                    <div class="singleSlide fade">
                         <img src="<?=$picture['image_gallery'];?>" alt="">
                     </div>
                     <?php
-                    }
+                    } 
+                    if(count($gallery) > 1) {?>
+                        <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+                        <a class="next" onclick="plusSlides(1)">&#10095;</a>
+                <?php }
                 } ?> 
+
+
             </div>
 
         <div class="post-content">
@@ -27,8 +33,6 @@
                 Posté le <?php the_date(); ?> par <?php the_author(); ?>.
             </p>
             </div>
-
-        </div>
     </div>
    
 
@@ -42,12 +46,26 @@
 
 
 
-    <?php endwhile; ?>
-    <?php endif; ?>
+    <?php } } ?>
     
-</div>
 
+<script>
+var slideIndex = 1;
+showSlides(slideIndex);
 
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
 
-
-
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("singleSlide");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  slides[slideIndex-1].style.display = "block";
+}
+</script>
