5edb21e6d8deaa55763928a551f37c98e392dc10
Plumtree3D <ham.in.kneesocks@gmail.com>
2021-11-24 09:52:55 +0100

    I'm going back to four o four

--- wp-content/themes/creative_agencies/404.php
+++ wp-content/themes/creative_agencies/404.php
@@ -3,8 +3,12 @@
 include('header.php');
 
 ?>
-<div class="container">
-    <h1>404</h1>
+
+
+<div class="fof">
+    <h1 id="fourofour">404</h1>
+    <p> Vous avez pris le mauvais chemin ! <br>
+Mais vous pouvez toujours <a href="#"> retourner à la page d'accueil... </a> </p>
 </div>
 
 <?php
